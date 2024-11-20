5592df1834743a3c08ae433d14e03d6e52c77101
Mohamedabdo273 <mohamedsaleh200330@gmail.com>
2024-11-20 21:51:02 +0200

    Update2

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -2,5 +2,12 @@
 {
     public class Hotel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string CoverImg { get; set; }
+        public List<ImageList> Images { get; set; }
     }
 }
